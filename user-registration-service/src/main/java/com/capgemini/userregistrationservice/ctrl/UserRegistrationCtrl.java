b17bfaef731ca6511ec492ea62c2d1aa84fe2f25
VykuntaraoGitHub <vykuntarao.e@gmail.com>
2025-08-24 22:17:43 +0530

    code added in ctrl calss

--- user-registration-service/src/main/java/com/capgemini/userregistrationservice/ctrl/UserRegistrationCtrl.java
+++ user-registration-service/src/main/java/com/capgemini/userregistrationservice/ctrl/UserRegistrationCtrl.java
@@ -1,4 +1,21 @@
 package com.capgemini.userregistrationservice.ctrl;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userCtrl")
 public class UserRegistrationCtrl {
+
+    @GetMapping("/getUser/{id}")
+    public String getUser(@PathVariable String id){
+        System.out.println("getUser---"+id);
+        String name="meena-Cap-HCL";
+       // String name2="Bavi";
+        System.out.println("getUser---"+name);
+        return name;
+
+    }
 }
