16bc742e72bea0ffefb416ab532f43190e8f55a5
VykuntaraoGitHub <144585061+VykuntaraoGitHub@users.noreply.github.com>
2025-08-24 21:40:27 +0530

    Add files via upload

---
+++ user-registration-service/src/main/java/com/capgemini/userregistrationservice/UserRegistrationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.capgemini.userregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationServiceApplication.class, args);
+        System.out.println("UserRegistrationServiceApplication started--------------------");
+        int i=10;
+        String name="meena";
+    }
+
+}
